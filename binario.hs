61378a7fd68e195612e5e10322460a9e3718c0fb
David Aparicio <davidalejandro1223@hotmail.com>
2017-09-05 13:41:45 -0500

    binario funcionando a la mitad

--- binario.hs
+++ binario.hs
@@ -1,3 +1,9 @@
+conversionDB :: Int -> String
+conversionDB 1 = "1"
+conversionDB n = (show(mod n 2))++(conversionDB(div n 2))
 
-binaDeci:: Int->Int
-binaDeci a = if (div a 2) == 1 then 1 else binaDeci(mod a 2)
+binario :: Int->String
+binario a = reverse(conversionDB a)
+
+conversionBD :: Int->Int
+conversionBD a = if a <=1 then a else (conversionBD(div a 10)*2) +(mod a 10)
